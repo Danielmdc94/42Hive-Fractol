fdf9195b112903e815248f26c403cb1b83e85cda
Daniel Palacio <dpalacio@student.hive.fi>
2022-05-17 16:51:43 +0300

    nother failed try at zoom

--- mouse.c
+++ mouse.c
@@ -6,7 +6,7 @@
 /*   By: dpalacio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:54:02 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/05/12 20:06:46 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/05/17 16:51:16 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ void	zoom(int button, int x, int y, t_data *data)
 {
 	if (button == 5)
 	{
-		data->r_min = data->r_min * 1.5;
-		data->r_max = data->r_max * 1.5;
-		data->i_min = data->i_min * 1.5;
-		data->i_max = data->i_max * 1.5;
+		data->r_min = data->r_min * 1.1;
+		data->r_max = data->r_max * 1.1;
+		data->i_min = data->i_min * 1.1;
+		data->i_max = data->i_max * 1.1;
 		if (data->max_iter > 50 && (data->max_iter - data->zoom) >= 50)
 			data->max_iter = data->max_iter - data->zoom;
 		data->zoom--;
 	}
 	if (button == 4)
 	{
-		data->r_min = data->r_min / 1.5;
-		data->r_max = data->r_max / 1.5;
-		data->i_min = data->i_min / 1.5;
-		data->i_max = data->i_max / 1.5;
+		data->r_min = data->r_min / 1.1;
+		data->r_max = data->r_max / 1.1;
+		data->i_min = data->i_min / 1.1;
+		data->i_max = data->i_max / 1.1;
 		if (data->max_iter < 500 && (data->max_iter + data->zoom) >= 50)
 			data->max_iter = data->max_iter + data->zoom;
 		data->zoom++;
